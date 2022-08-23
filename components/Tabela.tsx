a5386acd032d752b67e46f6427ccc65f42773fa5
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-08-22 22:46:28 -0400

    refactor: migração do código base

---
+++ components/Tabela.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { Estrela } from '../types';
+import Image from 'react-bootstrap/Image';
+
+export const Tabela: React.FC<{ estrelas: Estrela[] }> = ({ estrelas }) => {
+  return (
+    <Table striped bordered hover responsive>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Starred At</th>
+        </tr>
+      </thead>
+      <tbody>
+        {estrelas.map((estrela) => {
+          return (
+            <tr key={estrela.user.id}>
+              <td>
+                <Image
+                  fluid={true}
+                  rounded={true}
+                  width={50}
+                  height={50}
+                  src={estrela.user.avatar_url}
+                  alt="User avatar"
+                />
+              </td>
+              <td>{estrela.user.name}</td>
+              <td>{estrela.user.email}</td>
+              <td>{estrela.starred_at.valueOf()}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+};
---
+++ lib/github/components/SearchComponent.ts
@@ -0,0 +1,64 @@
+/*
+ *  Author: Hudson S. Borges
+ */
+import { get } from "lodash";
+
+import Component from "../Component";
+import Fragment from "../Fragment";
+import { SimplifiedRepositoryFragment } from "../fragments/RepositoryFragment";
+
+type Query = {
+  minStargazers?: number;
+  maxStargazers?: number;
+  language?: string;
+  name?: string;
+  sort?: "stars" | "created" | "updated" | undefined;
+  order?: "asc" | "desc" | undefined;
+};
+
+export default class SearchComponent extends Component {
+  constructor(query?: Query, after?: string, first?: number) {
+    super(null, "search");
+    this.includes.search = {
+      textFragment: "",
+      first,
+      after,
+      query: query ?? {},
+    };
+  }
+
+  get fragments(): Fragment[] {
+    return [SimplifiedRepositoryFragment];
+  }
+
+  toString(): string {
+    const searchQuery = get(this.includes, "search.query", {}) as Query;
+
+    let query = `stars:${searchQuery.minStargazers ?? 0}..${
+      searchQuery.maxStargazers ?? "*"
+    }`;
+
+    if (searchQuery.sort)
+      query += ` sort:${searchQuery.sort}${
+        searchQuery.order ? `-${searchQuery.order}` : ""
+      }`;
+
+    query += " sort:stars-desc sort:forks-desc";
+
+    if (searchQuery.language) query += ` language:${searchQuery.language}`;
+    if (searchQuery.name) query += ` repo:${searchQuery.name}`;
+
+    const args = super.argsToString({
+      first: get(this.includes, "search.first", 100),
+      after: get(this.includes, "search.after"),
+      query,
+    });
+
+    return `
+      ${this.alias}:search(${args}, type: REPOSITORY) {
+        pageInfo { hasNextPage endCursor }
+        nodes { ...${SimplifiedRepositoryFragment.code} }
+      }
+    `;
+  }
+}
