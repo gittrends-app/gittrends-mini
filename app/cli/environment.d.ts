93f24b2cc480c60fdfb5fe7073205c9cc81fdf07
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-10-06 13:06:46 -0400

    feat: permite usar postgres ou sqlite em produção

--- app/cli/environment.d.ts
+++ app/cli/environment.d.ts
@@ -1,25 +1,39 @@
 import { Knex as KnexOriginal } from 'knex';
 
+
 export {};
 
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'test' | 'development' | 'production';
-      CLI_POSTGRES_HOST?: string;
-      CLI_POSTGRES_PORT?: number;
-      CLI_POSTGRES_USERNAME?: string;
-      CLI_POSTGRES_PASSWORD?: string;
-      CLI_POSTGRES_DATABASE?: string;
-      CLI_POSTGRES_POOL_MIN?: string;
-      CLI_POSTGRES_POOL_MAX?: string;
+
+      CLI_API_URL?: string;
+      CLI_ACCESS_TOKEN?: string;
+
+      CLI_DATABASE?: 'sqlite' | 'postgres' | string;
+      CLI_DATABASE_HOST?: string;
+      CLI_DATABASE_PORT?: number;
+      CLI_DATABASE_USERNAME?: string;
+      CLI_DATABASE_PASSWORD?: string;
+      CLI_DATABASE_DB?: string;
+      CLI_DATABASE_POOL_MIN?: string;
+      CLI_DATABASE_POOL_MAX?: string;
+
       CLI_REDIS_HOST?: string;
       CLI_REDIS_PORT?: number;
       CLI_REDIS_DB?: number;
+
       CLI_QUEUE_ATTEMPS?: number;
-      CLI_ACCESS_TOKEN?: string;
-      CLI_API_URL?: string;
+
       CLI_WRITE_BATCH?: string;
+      CLI_WRITE_BATCH_STARGAZERS:? number;
+      CLI_WRITE_BATCH_WATCHERS:? number;
+      CLI_WRITE_BATCH_TAGS:? number;
+      CLI_WRITE_BATCH_RELEASES:? number;
+      CLI_WRITE_BATCH_DEPENDENCIES:? number;
+      CLI_WRITE_BATCH_ISSUES:? number;
+      CLI_WRITE_BATCH_PULL_REQUESTS:? number;
     }
   }
 }
--- app/cli/src/config/knex.config.ts
+++ app/cli/src/config/knex.config.ts
@@ -5,12 +5,21 @@ import { isNil, mapValues, omitBy, size } from 'lodash';
 import { homedir } from 'os';
 import { dirname, extname, resolve } from 'path';
 
-const isProduction = process.env.NODE_ENV === 'production';
-const baseDir = resolve(homedir(), '.gittrends', process.env.NODE_ENV || 'development');
+type TargetDatabase = 'sqlite' | 'postgres';
+
+if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
+
+if (process.env.CLI_DATABASE) {
+  process.env.CLI_DATABASE = process.env.CLI_DATABASE.toLowerCase();
+  if (!['sqlite', 'postgres'].includes(process.env.CLI_DATABASE)) throw new Error('Invalid database type');
+}
+
+const targetDatabase = (process.env.CLI_DATABASE || 'sqlite') as TargetDatabase;
+const baseDir = resolve(homedir(), '.gittrends', process.env.NODE_ENV);
 
 knex.QueryBuilder.extend('insertEntity', function (value: Record<string, unknown>) {
   const sqliteFormattedValue = mapValues(value, (value) => {
-    if (isProduction) {
+    if (targetDatabase === 'postgres') {
       // eslint-disable-next-line no-control-regex
       if (typeof value === 'string') return value.replace(/\u0000/g, '\\u0000');
       if (value instanceof Date) return value.toISOString();
@@ -18,15 +27,17 @@ knex.QueryBuilder.extend('insertEntity', function (value: Record<string, unknown
         if (size(value) === 0) return undefined;
         return JSON.stringify(value);
       }
-      return value;
     }
 
-    if (typeof value === 'boolean') return value.toString().toUpperCase();
-    if (value instanceof Date) return value.toISOString();
-    if (typeof value === 'object' || Array.isArray(value)) {
-      if (size(value) === 0) return undefined;
-      return JSON.stringify({ $Object: value });
+    if (targetDatabase === 'sqlite') {
+      if (typeof value === 'boolean') return value.toString().toUpperCase();
+      if (value instanceof Date) return value.toISOString();
+      if (typeof value === 'object' || Array.isArray(value)) {
+        if (size(value) === 0) return undefined;
+        return JSON.stringify({ $Object: value });
+      }
     }
+
     return value;
   });
 
@@ -34,57 +45,76 @@ knex.QueryBuilder.extend('insertEntity', function (value: Record<string, unknown
 });
 
 export async function getRepositoriesList(): Promise<string[]> {
-  if (isProduction) {
+  if (targetDatabase === 'postgres') {
     return createOrConnectDatabase('public')
       .then((conn) => conn.raw('SELECT schema_name FROM information_schema.schemata;').finally(() => conn.destroy()))
       .then(({ rows }) => rows.map((res: { schema_name: string }) => res.schema_name.replace(/\[dot\]/g, '.')))
       .then((names) => names.filter((name: string) => /.\/./gi.test(name)));
   }
 
-  const { found } = new GlobSync('**/*.sqlite', { cwd: baseDir });
-  return found.map((file) => file.replace(/\.sqlite$/i, ''));
+  if (targetDatabase === 'sqlite') {
+    const { found } = new GlobSync('**/*.sqlite', { cwd: baseDir });
+    return found.map((file) => file.replace(/\.sqlite$/i, ''));
+  }
+
+  throw new Error(`Invalid target database "${targetDatabase}"!`);
 }
 
 function knexResponseParser(result: any) {
   if (!result) return result;
   if (result.command && result.rows) return result;
-  if (isProduction) return omitBy(result, isNil);
-  return mapValues(omitBy(result, isNil), (value) => {
-    if (typeof value !== 'string') return value;
-    if (value === 'TRUE') return true;
-    else if (value === 'FALSE') return false;
-    else if (value.startsWith('{"$Object":')) return JSON.parse(value).$Object;
-    else return isProduction ? value.replace(/\\u0000/g, '\u0000') : value;
-  });
+
+  if (targetDatabase === 'postgres') {
+    return mapValues(omitBy(result, isNil), (value) => {
+      if (typeof value !== 'string') return value;
+      else return value.replace(/\\u0000/g, '\u0000');
+    });
+  }
+
+  if (targetDatabase === 'sqlite') {
+    return mapValues(omitBy(result, isNil), (value) => {
+      if (typeof value !== 'string') return value;
+      if (value === 'TRUE') return true;
+      else if (value === 'FALSE') return false;
+      else if (value.startsWith('{"$Object":')) return JSON.parse(value).$Object;
+      else return value;
+    });
+  }
+
+  throw new Error(`Invalid target database "${targetDatabase}"!`);
 }
 
 function getConnectionSettings(repo: string): Knex.Config<any> {
-  if (isProduction) {
+  if (targetDatabase === 'postgres') {
     return {
       client: 'pg',
       connection: {
-        host: process.env.CLI_POSTGRES_HOST ?? '127.0.0.1',
-        port: process.env.CLI_POSTGRES_PORT ?? 5432,
-        user: process.env.CLI_POSTGRES_USERNAME ?? 'root',
-        password: process.env.CLI_POSTGRES_PASSWORD ?? 'root',
-        database: process.env.CLI_POSTGRES_DATABASE ?? 'gittrends.app',
+        host: process.env.CLI_DATABASE_HOST ?? '127.0.0.1',
+        port: process.env.CLI_DATABASE_PORT ?? 5432,
+        user: process.env.CLI_DATABASE_USERNAME ?? 'root',
+        password: process.env.CLI_DATABASE_PASSWORD ?? 'root',
+        database: process.env.CLI_DATABASE_DB ?? 'gittrends.app',
       },
       searchPath: [repo.replace(/\./g, '[dot]')],
       pool: {
-        min: parseInt(process.env.CLI_POSTGRES_POOL_MIN || '1'),
-        max: parseInt(process.env.CLI_POSTGRES_POOL_MAX || '3'),
+        min: parseInt(process.env.CLI_DATABASE_POOL_MIN || '1'),
+        max: parseInt(process.env.CLI_DATABASE_POOL_MAX || '3'),
       },
     };
   }
 
-  const databaseFile = resolve(baseDir, ...repo.toLowerCase().split('/')) + '.sqlite';
-  mkdirSync(dirname(databaseFile), { recursive: true });
-  return {
-    client: 'better-sqlite3',
-    acquireConnectionTimeout: 60000,
-    useNullAsDefault: true,
-    connection: { filename: databaseFile },
-  };
+  if (targetDatabase === 'sqlite') {
+    const databaseFile = resolve(baseDir, ...repo.toLowerCase().split('/')) + '.sqlite';
+    mkdirSync(dirname(databaseFile), { recursive: true });
+    return {
+      client: 'better-sqlite3',
+      acquireConnectionTimeout: 60000,
+      useNullAsDefault: true,
+      connection: { filename: databaseFile },
+    };
+  }
+
+  throw new Error(`Invalid target database "${targetDatabase}"!`);
 }
 
 export async function createOrConnectDatabase(repo: string, _migrate = true) {
@@ -102,7 +132,8 @@ export async function createOrConnectDatabase(repo: string, _migrate = true) {
   });
 
   if (_migrate) {
-    if (isProduction) await conn.schema.createSchemaIfNotExists(repo.replace(/\./g, '[dot]').toLowerCase());
+    if (targetDatabase === 'postgres')
+      await conn.schema.createSchemaIfNotExists(repo.replace(/\./g, '[dot]').toLowerCase());
     await conn.migrate.latest();
   }
 
@@ -112,7 +143,8 @@ export async function createOrConnectDatabase(repo: string, _migrate = true) {
 export async function migrate(db: string | Knex): Promise<void> {
   if (typeof db === 'string') {
     const conn = await createOrConnectDatabase(db);
-    if (isProduction) await conn.schema.createSchemaIfNotExists(db.replace(/\./g, '[dot]').toLowerCase());
+    if (targetDatabase === 'postgres')
+      await conn.schema.createSchemaIfNotExists(db.replace(/\./g, '[dot]').toLowerCase());
     return migrate(conn).finally(() => conn.destroy());
   }
 
@@ -122,7 +154,8 @@ export async function migrate(db: string | Knex): Promise<void> {
 export async function rollback(db: string | Knex): Promise<void> {
   if (typeof db === 'string') {
     const conn = await createOrConnectDatabase(db);
-    if (isProduction) await conn.schema.createSchemaIfNotExists(db.replace(/\./g, '[dot]').toLowerCase());
+    if (targetDatabase === 'postgres')
+      await conn.schema.createSchemaIfNotExists(db.replace(/\./g, '[dot]').toLowerCase());
     return rollback(conn).finally(() => conn.destroy());
   }
 
