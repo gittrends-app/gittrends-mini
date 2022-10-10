2ff9b16930fcc9212e180c7866af0fe22b9343a9
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-10-10 15:24:29 -0400

    chore: altera estrategia de modificação da colun

--- app/cli/src/config/migrations/20221010191433_alter_repository_column_type.ts
+++ app/cli/src/config/migrations/20221010191433_alter_repository_column_type.ts
@@ -3,15 +3,11 @@ import { Knex } from 'knex';
 import { Repository } from '@gittrends/entities';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.alterTable(Repository.__collection_name, (table) => {
-    table.dropColumn('mirror_url');
-    table.text('mirror_url');
-  });
+  await knex.schema.alterTable(Repository.__collection_name, (table) => table.dropColumn('mirror_url'));
+  return knex.schema.alterTable(Repository.__collection_name, (table) => table.text('mirror_url').alter());
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.alterTable(Repository.__collection_name, (table) => {
-    table.dropColumn('mirror_url');
-    table.integer('mirror_url');
-  });
+  await knex.schema.alterTable(Repository.__collection_name, (table) => table.dropColumn('mirror_url'));
+  return knex.schema.alterTable(Repository.__collection_name, (table) => table.integer('mirror_url').alter());
 }
