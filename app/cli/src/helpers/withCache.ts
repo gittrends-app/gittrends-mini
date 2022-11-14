29761a0466bc3229b59f829cc68d9a796082f069
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-11-14 14:02:58 -0400

    fix: usa db específico para caching

--- app/cli/src/helpers/withCache.ts
+++ app/cli/src/helpers/withCache.ts
@@ -4,7 +4,7 @@ import { EntitiesCache } from '../cache/EntitiesCache';
 
 const REDIS_HOST = process.env.CLI_REDIS_HOST || 'localhost';
 const REDIS_PORT = parseInt(process.env.CLI_REDIS_PORT || '6379');
-const REDIS_DB = parseInt(process.env.CLI_REDIS_DB || '1');
+const REDIS_DB = parseInt(process.env.CLI_CACHE_DB || '1');
 
 export async function withCache<T = any>(callback: (queue: Cache) => Promise<T>): Promise<T>;
 export function withCache(): Cache;
