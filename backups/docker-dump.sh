909734face5108c39ff1236327836ebf2b17572a
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-11-27 16:58:19 -0300

    chore: remove script de backup

--- backups/docker-dump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker run --rm -v "$(pwd)/files:/backups" \
-  -u "$(id -u):$(id -g)" \
-  -e POSTGRES_HOST=postgres \
-  -e POSTGRES_DB=${POSTGRES_DB:-gittrends.app} \
-  -e POSTGRES_USER=${POSTGRES_USER:-root} \
-  -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-root} \
-  prodrigestivill/postgres-backup-local /backup.sh
