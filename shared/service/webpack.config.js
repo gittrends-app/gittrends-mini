1604eb982df5b6e46edaebef03208a97c24401cb
Hudson Silva Borges <hudsonsilbor@gmail.com>
2023-09-30 09:50:51 -0400

    feat: adiciona webpack nos pacotes compartilhados

---
+++ shared/service/webpack.config.js
@@ -0,0 +1,27 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, './tsconfig.json'),
+      }),
+    ],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
