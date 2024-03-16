899d3010dbe76b4acce26459f1da4ae790e84378
Hudson S. Borges <hudsonsilbor@gmail.com>
2024-03-16 15:44:15 -0300

    chore: remove pacote web por não ser prioridade

--- app/web/pouch.config.ts
+++
@@ -1,7 +0,0 @@
-import PouchDB from 'pouchdb-browser';
-import PouchFind from 'pouchdb-find';
-import PouchUpsert from 'pouchdb-upsert';
-
-PouchDB.plugin(PouchFind).plugin(PouchUpsert);
-
-export default PouchDB;
